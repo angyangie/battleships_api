775d9d4ca3cc49752261c9ed8cc4012d73f8399a
Michael Appell <mikeappell@gmail.com>
2016-06-21 12:54:44 -0400

    Win loss works

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -7,7 +7,27 @@ module Api
       end
 
       def show
-        render json: Game.includes(:user_games, :ships, :users).find_by(id: params[:id]), include: ['user_games', 'ships', 'users']
+        game = Game.find(1)
+        game.completed = 0
+        game.user_games.destroy_all
+        Ship.destroy_all
+        # user_game1 = UserGame.find(1)
+        # user_game2 = UserGame.find(2)
+        # game.user_games.each do |ug|
+        #   user
+        #   # ug.ships.destroy_all
+        #   ug.generate_random_ships
+        # end
+        user_game1 = UserGame.create(id: 1, user: User.first, game: game)
+        user_game2 = UserGame.create(id: 2, user: User.second, game: game)
+        # binding.pry
+               # user1 = User.create(username: "angyangie", first_name: "Angie", last_name: "Ramirez", email: "angie.ramirez12@gmail.com", player_rank: "10", password: "hellothere1")
+        # user2 = User.create(username: "mykemike", first_name: "Michael", last_name: "Appell", email: "mikeappell@gmail.com", player_rank: "3", password: "hellothere1")
+        game.save
+        # puts "START"
+        # puts Game.includes(:user_games, :ships, :users).find(1).ships
+        # puts "END"
+        render json: Game.includes(:user_games, :ships, :users).find(1), include: ['user_games', 'ships', 'users']
       end
 
       def hits_array
@@ -17,13 +37,14 @@ module Api
       end
 
       def new_game
-        game = Game.find_by(id: params[:id])
+        # binding.pry
+        game = Game.find_by(id: params[:game_id])
         user1 = User.create(username: "angyangie", first_name: "Angie", last_name: "Ramirez", email: "angie.ramirez12@gmail.com", player_rank: "10", password: "hellothere1")
         user2 = User.create(username: "mykemike", first_name: "Michael", last_name: "Appell", email: "mikeappell@gmail.com", player_rank: "3", password: "hellothere1")
-        user_game1 = UserGame.create(user: user1, game: game1)
-        user_game2 = UserGame.create(user: user2, game: game1)
+        user_game1 = UserGame.create(user: user1, game: game)
+        user_game2 = UserGame.create(user: user2, game: game)
         game.save
-        render json: game.includes(:user_games, :ships, :users), include: ['user_games', 'ships', 'users']
+        render json: Game.includes(:user_games, :ships, :users).find_by(id: params[:game_id]), include: ['user_games', 'ships', 'users']
       end
     end
   end
